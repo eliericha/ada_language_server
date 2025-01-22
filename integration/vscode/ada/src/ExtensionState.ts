2b81b949990f8ecf59f0b83f8c1281f39cb3af57
Elie Richa <richa@adacore.com>
2025-01-22 17:41:15 +0000

    First attempt at running GNATtest tests in coverage mode
    For eng/ide/ada_language_server#1315

--- integration/vscode/ada/src/ExtensionState.ts
+++ integration/vscode/ada/src/ExtensionState.ts
@@ -402,6 +402,15 @@ export class ExtensionState {
     public getSparkTaskProvider() {
         return this.sparkTaskProvider;
     }
+
+    /**
+     *
+     * @returns the Ada task provider which can be useful for resolving tasks
+     * created on the fly, e.g. when running GNATtest tests in coverage mode.
+     */
+    public getAdaTaskProvider() {
+        return this.adaTaskProvider;
+    }
 }
 
 /**
